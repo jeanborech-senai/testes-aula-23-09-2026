2b61714bb847de1b30ad9ba6afc224a7c08f73e9
TESTES <jean.borech@edu.sc.senai.br>
2026-09-23 21:24:08 -0300

    teste branch

--- js/teste.js
+++ js/teste.js
@@ -2,7 +2,3 @@ console.log("Hello, World!");
 console.log("Hello, World!");
 console.log("Hello, World!");
 console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
-console.log("Hello, World!");
